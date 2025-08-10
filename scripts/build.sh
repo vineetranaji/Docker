51ccf01a2e03d31393c3859daa368dd82bca6ea1
Vineet Rana <vineetrana2208@gmail.com>
2025-08-10 13:22:25 +0530

    add

--- scripts/build.sh
+++ scripts/build.sh
@@ -1,3 +1,8 @@
-docker build -t my-app ./dockerfiles
+#!/bin/bash
+set -e
 
+# Build the Docker image using a specific Dockerfile
+docker build -t my-app -f ./dockerfiles/Dockerfile-node ./dockerfiles
+
+# Run the container
 docker run -d -p 3000:3000 my-app
--- scripts/cleanup.sh
+++ scripts/cleanup.sh
@@ -1,4 +1,9 @@
 
 # Remove unused Docker containers, images, and volumes
-docker system prune -af
+read -p "This will remove all unused Docker containers, images, and volumes. Continue? (y/N): " confirm
+if [[ "$confirm" =~ ^[Yy]$ ]]; then
+  docker system prune -af
+else
+  echo "Cleanup aborted."
+fi
 
